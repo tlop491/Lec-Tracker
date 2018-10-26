76a449d33db4810867b53c069442fe06aef2868a
Timothy Lopez <tlop491@aucklanduni.ac.nz>
2018-10-26 16:22:22 +1300

    Checking for URL: Mediastore.ac.nz and counting

--- LecTrack/content.js
+++ LecTrack/content.js
@@ -9,9 +9,22 @@
 // })
 
 //Create new regular expression 
-const regularExpression = new RegExp('bear', 'gi')
-const matches = document.documentElement.innerHTML.match(regularExpression)
+const isLectureRecordingIdentifier = new RegExp('mediastore.auckland.ac.nz', 'gi')
+const matches = window.location.href.match(isLectureRecordingIdentifier)
+
+
+
 chrome.runtime.sendMessage({
+
+    var urlMediastore = window.location.href
+
+for (int i = 0; i < urlMediastore.length; i++)  {
+    if(isUpperCase(input.charAt(i))
+    {
+        console.log(input.charAt(i))
+    }
+}
+
     url: window.location.href, 
     count: matches.length
 })
